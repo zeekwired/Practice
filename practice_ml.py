d7df7e6efdad1e55f92cf2dcf9406264f914f49c
Zach Dawson <44974963+zeekwired@users.noreply.github.com>
2023-04-14 18:18:02 -0500

    fsasag

---
+++ practice_ml.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
--- pratice.py
+++ pratice.py
@@ -1,25 +1,67 @@
-import random
+import dash 
+from dash import dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+from flask import Flask
 
-# Create a variable to store the day of the week
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
 
-day = input("What day is it? ")
+import os
+import json
 
-# Creating a dictionary to store the days of the week and the meals for each day
-meals = {
-    "Monday": ['Chicken', 'Rice', 'Beans'],
-    "Tuesday": ['Beef', 'Rice', 'Beans'],
-    "Wednesday": ['Fish', 'Rice', 'Beans'],
-    "Thursday": ['Chicken', 'Rice', 'Beans'],
-    "Friday": ['Beef', 'Rice', 'Beans'],
-    "Saturday": ['Fish', 'Rice', 'Beans'],
-    "Sunday": ['Chicken', 'Rice', 'Beans']
-}
+# Loading data 
+df = pd.read_csv(r'Copy of TS2 Find At Location LOG - APR.csv')
 
-# Creating a variable to store a random number to select a meal
-random_number = random.randint(0, 3)
+# Initializing app
+server = Flask(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], server=server)
 
-# Creating a variable that uses the random number to select a meal
-meal = meals[day][random_number]
+# Creating app layout 
 
-# Printing the meal
-print(meal)
+app.layout = dbc.Container([
+
+    # Creating Header for Application
+    dbc.Row([
+        # Creating a header using dbc.Label
+        dbc.Label('Machine Learning Dashboard', className='h1', style={'text-align': 'center', 'margin-top': '12px'})
+            ]),
+    # End of Row 1
+
+    # Creating a Row with 2 columns for graphs
+    dbc.Row([
+    
+        # Column 1 for Graph 1
+        dbc.Col([
+                # Creating a header using dbc.Label
+                dbc.Label('Graph 1', className='h2', style={'text-align': 'center', 'margin-top': '12px'}),
+                # Creating a Graph using dcc.Graph
+                dcc.Graph(id='graph1', figure={})
+                ]),
+                # End of Column 1
+
+        # Column 2 for Graph 2
+        dbc.Col([
+                # Creating a header using dbc.Label
+                dbc.Label('Graph 2', className='h2', style={'text-align': 'center', 'margin-top': '12px'}),
+                # Creating a Graph using dcc.Graph
+                dcc.Graph(id='graph2', figure={})
+                ])
+                # End of Column 2
+
+            ]),
+        # End of Row 2
+
+])
+
+
+
+
+# Creating callbacks for widgets in application
+
+# Running app
+if __name__ == '__main__':
+    app.run_server(debug=True)
